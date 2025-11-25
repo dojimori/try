c7acace9f25784c8ba30c0aaa8d567feb2b50752
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-11-25 22:37:16 +0800

    style: change chat transition to slide from left

--- client/src/views/chat-view.vue
+++ client/src/views/chat-view.vue
@@ -23,7 +23,10 @@
       <!-- chat-body -->
       <div class="h-[60vh] p-2 flex flex-col gap-3 overflow-y-scroll" ref="chatBox">
         <div
-          v-motion-slide-bottom
+          v-motion
+          :initial="{ opacity: 0, x: -50 }"
+          :enter="{ opacity: 1, x: 0 }"
+          :duration="400"
           v-for="(data, index) in messages"
           :key="index"
           class="flex flex-col p-1.5 gap-1"
@@ -116,11 +119,14 @@ input:focus {
   animation: lift 0.3s forwards;
 }
 
-.emoji-btn {
+.emoji-btn,
+img {
   transition: all 0.3s ease;
+  cursor: pointer;
 }
 
-.emoji-btn:hover {
+.emoji-btn:hover,
+img:hover {
   scale: 1.2;
   transform: rotate(-15deg);
 }
