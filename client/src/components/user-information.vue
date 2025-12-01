b8c78f17be8227d1b85c9b2383db5d2c822f3767
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-01 08:27:32 +0800

    added more information template

--- client/src/components/user-information.vue
+++ client/src/components/user-information.vue
@@ -1,16 +1,78 @@
 <template>
   <!-- user profile section -->
-  <div class="bg-white border border-gray-200 p-4 shadow-lg md:w-[250px] w-full">
+  <div
+    class="bg-white border border-gray-200 p-4 shadow-lg md:w-[250px] w-full overflow-y-auto"
+  >
     <div>
-      <h2 class="font-bold text-[#29487d] mb-4 border-b border-gray-300 pb-2">Me</h2>
-      <div class="flex flex-col gap-2"></div>
+      <div class="mb-2 flex flex-row items-center justify-between">
+        <h2 class="font-bold text-[#29487d]">Me</h2>
+        <button
+          class="bg-slate-300 px-3 py-1.5 border border-slate-400 text-[#29487d] font-bold cursor-pointer flex items-center gap-2 hover:bg-slate-200 hover:translate-y-[-3px] duration-200"
+        >
+          <ph-pencil-simple></ph-pencil-simple>
+          edit profile
+        </button>
+      </div>
+      <div class="border-t border-gray-400 mb-2"></div>
     </div>
 
-    <div v-if="user">
+    <div v-if="user" class="flex flex-col items-center justify-center">
+      <!-- pfp -->
       <img src="/def_pfp_6.jpg" class="pfp" />
-
-      <div>
+      <!-- username -->
+      <h1 class="mt-4 font-bold">
         {{ user.username }}
+        <small class="block font-normal text-gray-600">@{{ user.username }}</small>
+      </h1>
+      <!-- bio -->
+      <div class="my-4 border-b border-gray-400 w-full text-center">
+        <span><i>no bio yet...</i></span>
+      </div>
+      <!-- end bio -->
+
+      <!-- user info's -->
+      <div class="w-full flex flex-col gap-3">
+        <!-- joined at -->
+        <div class="flex justify-between">
+          <span class="font-bold text-gray-600">joined at</span>
+          <span>11/24/2025</span>
+        </div>
+
+        <!-- gender -->
+        <div class="flex justify-between">
+          <span class="font-bold text-gray-600">gender</span>
+          <span>-</span>
+        </div>
+
+        <!-- country -->
+        <div class="flex justify-between">
+          <span class="font-bold text-gray-600">country</span>
+          <span>-</span>
+        </div>
+
+        <!-- relationships -->
+        <div class="flex justify-between">
+          <span class="font-bold text-gray-600">relationships</span>
+          <span>-</span>
+        </div>
+
+        <!-- interested in -->
+        <div class="flex justify-between">
+          <span class="font-bold text-gray-600">interested in</span>
+          <span>-</span>
+        </div>
+
+        <!-- likes -->
+        <div class="flex justify-between">
+          <span class="font-bold text-gray-600">likes</span>
+          <span>-</span>
+        </div>
+
+        <!-- dislikes -->
+        <div class="flex justify-between">
+          <span class="font-bold text-gray-600">dislikes</span>
+          <span>-</span>
+        </div>
       </div>
     </div>
   </div>
@@ -24,12 +86,18 @@
 
 <script>
 import { getMe } from "@/utils/user";
+// import { PhPencilSimple } from "@phosphor-icons";
+import { PhPencilSimple } from "@phosphor-icons/vue";
 
 export default {
   data() {
     return {
       user: null,
     };
+  },
+
+  components: {
+    PhPencilSimple,
   },
 
   async mounted() {
