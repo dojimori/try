48aa673686e686b7cb2078d30db7fe4e89deb384
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-16 12:52:11 +0800

    im too lazy to do this

--- client/src/views/profile-view.vue
+++ client/src/views/profile-view.vue
@@ -1,4 +1,40 @@
 <template>
+    <main class="flex flex-col gap-0 border border-gray-400 sm:w-3xl lg:w-[1080px]">
+        <header-component></header-component>
+
+        <div class="bg-white p-2" v-motion-fade>
+
+            <div class="flex gap-12">
+                <div class=" border-2 border-gray-300 p-2">
+                    <img src="/def_pfp_3.jpg" class="w-[166px]" alt="">
+                </div>
+
+                <div class="mt-4">
+                    <h4 style="font-size: 1.2em; ">Username</h4>
+                    <!-- <h4 style="font-size: 1.2em; ">2/2/2</h4> -->
+                </div>
+            </div>
 
 
+
+            <div>
+                <h1>timeline</h1>
+            </div>
+
+        </div>
+
+
+
+    </main>
 </template>
+
+<script>
+import HeaderComponent from '@/components/header-component.vue';
+
+export default {
+    components: {
+        HeaderComponent
+    }
+}
+
+</script>
